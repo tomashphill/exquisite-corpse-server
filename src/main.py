a7fffb86f434fc5b167bfcb8b36628b94f36735d
tomashphill <tomashphill@gmail.com>
2021-04-07 03:11:51 -0400

    Changed procfile

--- src/main.py
+++ src/main.py
@@ -4,7 +4,6 @@ import io
 
 
 from fastapi import Depends, FastAPI, HTTPException
-from fastapi.middleware.cors import CORSMiddleware
 from pydantic.main import BaseModel
 from starlette.responses import StreamingResponse
 from sqlalchemy.orm import Session
@@ -17,19 +16,6 @@ from .sql.database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-origins = [
-    "http://localhost",
-    "http://localhost:8080"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 def get_db():
